30efb4407c918f37ec8f4a20b1387deab2a937d3
ashishverma94 <hariashish1@gmail.com>
2024-10-08 14:52:38 +0530

    removed all components from page

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,23 @@
 
 import "./App.css";
 // import Footer from "./components/Footer";
-import Grid from "./components/Grid";
-import Categories from "./components/Categories";
-import Preorder from "./components/Preorder";
-import Navbar from "./components/Navbar";
-import HeroSection from "./components/HeroSection";
+// import Grid from "./components/Grid";
+// import Categories from "./components/Categories";
+// import Preorder from "./components/Preorder";
+// import Navbar from "./components/Navbar";
+// import HeroSection from "./components/HeroSection";
 
 function App() {
 
   return (
     <div className="w-[100%] flex flex-col  justify-between  p-[50px] gap-[40px]">
-      <Navbar />
+      {/* <Navbar />
       <HeroSection/>
       <Categories />
       <Grid />
-      <Preorder />
+      <Preorder /> */}
       {/* <Footer /> */}
+      <span className="text-[25px] font-bold">Hello from Home Page</span>
     </div>
   );
 }
